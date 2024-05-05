5445f83bf43315babe10d60cf86f2fbc6f24c758
CryptoNahue <cryptonahue@gmail.com>
2024-05-05 14:14:59 -0300

    feat: add statick translation and hide tags

---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const ui = {
+  en: {
+    "site.title": "Astro Blog",
+    "home.title": "HomeEN",
+    "site.description": "Welcome to my website!",
+    "nav.home": "Home",
+  },
+  pt: {
+    "site.title": "Astro Blog",
+    "home.title": "HomePT",
+    "site.description": "Bienvenue sur mon site web !",
+    "nav.home": "Accueil",
+  },
+  es: {
+    "site.title": "Astro Blog",
+    "home.title": "HomeES",
+    "site.description": "Bienvenido a mi sitio web!",
+    "nav.home": "Inicio",
+  },
+} as const;
