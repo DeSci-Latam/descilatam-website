829a1525fa27f8029ab00951a92ae9779a0bcb45
CryptoNahue <cryptonahue@gmail.com>
2024-05-05 13:10:08 -0300

    feat: base new web

---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig, squooshImageService } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { remarkReadingTime } from './src/utils/readTime.ts'
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://descilatam.org/', // Write here your website url
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		},
+
+		image: {
+			service: squooshImageService(),
+		  },
+	},
+	integrations: [
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				experimentalThemes: {
+					light: 'vitesse-light',
+					dark: 'material-theme-palenight',
+				  },
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind()
+	]
+})
