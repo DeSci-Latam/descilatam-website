a37493152ee5b15eeaec63e5100b032cbb615f22
CryptoNahue <cryptonahue@gmail.com>
2024-05-08 18:09:50 -0300

    feat: first version

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,20 +1,24 @@
 export const ui = {
   en: {
-    "site.title": "Astro Blog",
+    "site.title": "The World of DeSci",
     "home.title": "HomeEN",
     "site.description": "Welcome to my website!",
     "nav.home": "Home",
+    "home.tagline": "Promoting the DeSci movement around the world.",
   },
   pt: {
-    "site.title": "Astro Blog",
+    "site.title": "The World of DeSci",
     "home.title": "HomePT",
     "site.description": "Bienvenue sur mon site web !",
     "nav.home": "Accueil",
+    "home.tagline": "Promovendo o movimento DeSci ao redor do mundo.",
+
   },
   es: {
-    "site.title": "Astro Blog",
+    "site.title": "The World of DeSci",
     "home.title": "HomeES",
     "site.description": "Bienvenido a mi sitio web!",
     "nav.home": "Inicio",
+    "home.tagline": "Promocionando el movimiento DeSci alrededor del mundo.",
   },
 } as const;
